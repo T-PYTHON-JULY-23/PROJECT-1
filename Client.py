2d52bd7ae707fb450968c68622e9753abedfd381
AtheerAbdullah98 <140397937+AtheerAbdullah98@users.noreply.github.com>
2023-08-13 00:42:14 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -39,22 +39,24 @@ def ClientInfo():
 def ClientInput():
 
     clientlist = ClientInfo()
+    exit = True
     account = AccountManagement(clientlist[0],clientlist[1],clientlist[2],clientlist[3],clientlist[4],clientlist[5],clientlist[6],clientlist[7],clientlist[8],clientlist[9])
     try:  
         print('_'*30)
-        UserChoese = input("\nPlease Choose from  number from the list below:\n1- To Calculat all Last Year Spending.\n2- To Estimate Next Year Spending.\n3- To Get Information On Your Debit.\n4- To Calculat How Many Years Left For Your Retirement.\n5- To Calculat The Needed Raise In Your Salary.\n6- To Exit The Programme.\n")
-        if UserChoese == '1':
-            print("All your last year spending: "+account.CalculatYearSpending()+"\n")
-        elif UserChoese == '2':
-            print("Estimate all of next year spending: "+account.EstimatingSpending()+"\n")
-        elif UserChoese == '3':
-            account.DebitInfo()
-        elif UserChoese == '4':
-            account.TimeLeftRetirement()
-        elif UserChoese == '5':
-            account.SalaryCal()
-        elif UserChoese == '6':
-            exit()    
+        while exit:
+            UserChoese = input("\nPlease Choose from  number from the list below:\n1- To Calculat all Last Year Spending.\n2- To Estimate Next Year Spending.\n3- To Get Information On Your Debit.\n4- To Calculat How Many Years Left For Your Retirement.\n5- To Calculat The Needed Raise In Your Salary.\n6- To Exit The Programme.\n")
+            if UserChoese == '1':
+                print("All your last year spending: "+account.CalculatYearSpending()+"\n")
+            elif UserChoese == '2':
+                print("Estimate all of next year spending: "+account.EstimatingSpending()+"\n")
+            elif UserChoese == '3':
+                account.DebitInfo()
+            elif UserChoese == '4':
+                account.TimeLeftRetirement()
+            elif UserChoese == '5':
+                account.SalaryCal()
+            elif UserChoese == '6':
+                exit = False    
     except Exception:
         print("Try Again!!\n")
         ClientInput()
