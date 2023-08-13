137a25f9e4aba492824ed8d0db8e54885e2b79ab
sarahnaghi <sara.naghi@gmail.com>
2023-08-13 10:23:59 +0300

    project-1

--- Pet/pet.py
+++ Pet/pet.py
@@ -1,10 +1,9 @@
 class Pet:
 
-    def __init__(self, color:str , bread:str ,type:str, gender:str,contact_info:str,period:str)->None:
+    def __init__(self, color:str , bread:str ,type:str, gender:str,contact_info:str)->None:
         self.color = color
         self.bread = bread
         self.type = type
         self.gender = gender
         self.contact_info = contact_info
-        self.period =period
         
